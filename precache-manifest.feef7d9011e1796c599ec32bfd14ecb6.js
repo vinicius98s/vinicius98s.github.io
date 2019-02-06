0b99f1dddc5d856310881e5fc7188a5d4fdb49fc
Vinicius Sales <vinicius.sales@aunica.local>
2019-02-06 21:04:33 -0200

    Development build
    First commit for development info.

---
+++ precache-manifest.feef7d9011e1796c599ec32bfd14ecb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8f0ccd768759f66750e1",
+    "url": "/static/js/main.8f0ccd76.chunk.js"
+  },
+  {
+    "revision": "4844dc8e2dafa9c2cef0",
+    "url": "/static/js/1.4844dc8e.chunk.js"
+  },
+  {
+    "revision": "8f0ccd768759f66750e1",
+    "url": "/static/css/main.4c94ad9e.chunk.css"
+  },
+  {
+    "revision": "c87d872ad18b2d75b9ebcd149e578c99",
+    "url": "/index.html"
+  }
+];
